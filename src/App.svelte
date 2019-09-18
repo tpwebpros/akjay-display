1fb9f64158686686f75c8350e69f99b9561fbe1f
Eric Watson <wasnotrice@gmail.com>
2019-09-18 08:25:51 -0500

    Remove old code

--- src/App.svelte
+++ src/App.svelte
@@ -13,7 +13,7 @@
 
   export let server;
 
-  let client = new Client(server);
+  const client = new Client(server);
 
   const router = new Router({
     "/": GamePicker,
@@ -25,18 +25,6 @@
   });
 
   let route;
-
-  let state = {
-    maxScore: 100,
-    players: [
-      { name: "alice", avatar: "mushroom", score: 0 },
-      { name: "bob", avatar: "squid", score: 0 },
-      { name: "charlie", avatar: "taco", score: 0 },
-      { name: "dot", avatar: "cactus", score: 0 },
-      { name: "ev", avatar: "owl", score: 0 }
-    ]
-  };
-
   let updateInterval = 3000; // 3 seconds
 
   beforeUpdate(() => {
