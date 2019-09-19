b24556b2e79bbeede74b41826613ec83efa589dc
Eric Watson <wasnotrice@gmail.com>
2019-09-18 22:55:21 -0500

    Extract score

---
+++ src/Score.svelte
@@ -0,0 +1,34 @@
+<script>
+  export let score;
+</script>
+
+<style>
+  .score {
+    display: flex;
+    flex-flow: column nowrap;
+    align-items: center;
+    margin: 0.2em 1em 0.4em;
+  }
+
+  .score :first-child {
+    background-color: #444;
+    border-radius: 0.5em;
+    color: #eee;
+    display: inline-block;
+    font-size: 1rem;
+    font-weight: 700;
+    height: 1.4em;
+    padding: 0.3em;
+    position: relative;
+    text-align: center;
+  }
+
+  .score :nth-child(2) {
+    font-size: 0.8rem;
+  }
+</style>
+
+<span class="score">
+  <span>{score}</span>
+  <span>score</span>
+</span>
--- src/Team.svelte
+++ src/Team.svelte
@@ -1,5 +1,6 @@
 <script>
   import Avatar from "./Avatar.svelte";
+  import Score from "./Score.svelte";
   export let avatar;
   export let name;
   export let score;
@@ -18,31 +19,6 @@
     font-weight: 700;
     margin-top: 0.4rem;
   }
-
-  .score {
-    display: flex;
-    flex-flow: column nowrap;
-    align-items: center;
-    margin: 0.2em 1em 0.4em;
-  }
-
-  .score :first-child {
-    background-color: #444;
-    border-radius: 1.4em;
-    color: #eee;
-    display: inline-block;
-    font-size: 1rem;
-    font-weight: 700;
-    height: 1.4em;
-    width: 1.4em;
-    padding: 0.3em;
-    position: relative;
-    text-align: center;
-  }
-
-  .score :nth-child(2) {
-    font-size: 0.8rem;
-  }
 </style>
 
 <div class="team">
@@ -50,10 +26,7 @@
     <Avatar {avatar} size={avatarSize} />
     <span class="name">{name}</span>
     {#if score != null}
-      <span class="score">
-        <span>{score}</span>
-        <span>score</span>
-      </span>
+      <Score {score} />
     {/if}
   {/if}
 </div>
